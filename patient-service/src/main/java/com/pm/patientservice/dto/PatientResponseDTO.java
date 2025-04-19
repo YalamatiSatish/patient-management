a0a245a77109425c1c41ecb878f2d12ba10e5ed5
YalamatiSatish <satishyalamati18@gmail.com>
2025-04-19 09:04:04 +0200

    Initial commit

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,60 @@
+package com.pm.patientservice.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class PatientResponseDTO {
+    
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private LocalDate dateOfBirth;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
